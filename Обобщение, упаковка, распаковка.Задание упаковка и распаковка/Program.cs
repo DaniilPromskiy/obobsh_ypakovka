7ace22a6ca0733e995b8d091d55f453c5edc1f57
daniilPromskiy <daniil.promskiy2006@gmail.com>
2025-02-11 19:14:46 +0300

    main

---
+++ Обобщение, упаковка, распаковка.Задание упаковка и распаковка/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<object> randomValues = new List<object>();
+
+        Random random = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            if (random.Next(2) == 0) 
+            {
+                float randomFloat = (float)(random.NextDouble() * 100); 
+                randomValues.Add(randomFloat);
+            }
+            else
+            {
+                int randomInt = random.Next(1, 101); 
+                randomValues.Add(randomInt);
+            }
+        }
+
+        Console.WriteLine("Сгенерированные значения:");
+        foreach (var value in randomValues)
+        {
+            Console.WriteLine(value);
+        }
+
+        double sum = SumValues(randomValues);
+        Console.WriteLine($"Сумма значений: {sum}");
+    }
+
+    static double SumValues(List<object> values)
+    {
+        double sum = 0;
+
+        foreach (var value in values)
+        {
+            if (value is int intValue)
+            {
+                sum += intValue;
+            }
+            else if (value is float floatValue)
+            {
+                sum += floatValue;
+            }
+        }
+
+        return sum;
+    }
+}
